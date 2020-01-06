ac7eb56e9e9243f3f91a7a5f38aaef4a9ccae634
Zazsona <40435178+Zazsona@users.noreply.github.com>
2020-01-06 18:36:04 +0000

    Add ListViewModel.

---
+++ app/src/main/java/com/zazsona/todo/viewmodel/ListViewModel.java
@@ -0,0 +1,29 @@
+package com.zazsona.todo.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.zazsona.todo.model.database.Todo;
+import com.zazsona.todo.model.repository.TodoRepository;
+
+import java.util.List;
+
+public class ListViewModel extends AndroidViewModel
+{
+    private TodoRepository todoRepository;
+
+    public ListViewModel(@NonNull Application application)
+    {
+        super(application);
+        todoRepository = new TodoRepository(application);
+    }
+
+    public LiveData<List<Todo>> getTodos()
+    {
+        return todoRepository.getTodos();
+    }
+
+}
