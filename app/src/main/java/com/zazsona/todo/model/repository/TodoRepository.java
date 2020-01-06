59da5baa5bbb9217fe071f5667bf55a5d93df31f
Zazsona <40435178+Zazsona@users.noreply.github.com>
2020-01-06 18:34:48 +0000

    Implement threading for database operations in the repository

--- app/src/main/java/com/zazsona/todo/model/repository/TodoRepository.java
+++ app/src/main/java/com/zazsona/todo/model/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package com.zazsona.todo.model.repository;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 import com.zazsona.todo.model.database.Todo;
@@ -36,28 +37,37 @@ public class TodoRepository
      * @param id the id of the Todo
      * @return the todo
      */
-    public LiveData<Todo> getTodo(UUID id)
+    public LiveData<Todo> getTodo(String id)
     {
         return todoDAO.getTodoById(id);
     }
 
     /**
      * Adds a {@link Todo} to the {@link TodoDatabase}
-     * @param id a unique ID for the Todo
      * @param name the todo title
      * @param description the todo detail
      * @param complete boolean on complete.
      */
-    public void addTodo(UUID id, String name, String description, boolean complete)
+    public void addTodo(final String name, final String description, final boolean complete)
     {
-        todoDAO.addTodo(new Todo(id, name, description, (System.currentTimeMillis()/1000), complete)); //Single point of truth for adding Todos. Set time here.
+        Todo todo = new Todo(UUID.randomUUID().toString(), name, description, (System.currentTimeMillis() / 1000), complete); //Single point of truth for adding Todos. Set time here.
+        new DatabaseAsyncTask(todoDAO, true, false, false).execute(todo);
+    }
+
+    /**
+     * Adds a {@link Todo} to the {@link TodoDatabase}
+     * @param todo the Todo to add
+     */
+    public void addTodo(Todo todo)
+    {
+        new DatabaseAsyncTask(todoDAO, true, false, false).execute(todo);
     }
 
     /**
      * Removes the {@link Todo} defined by the ID from the {@link TodoDatabase}
      * @param id the UUID for the Todo
      */
-    public void removeTodo(UUID id)
+    public void removeTodo(String id)
     {
         todoDAO.removeTodo(id);
     }
@@ -71,5 +81,41 @@ public class TodoRepository
         todoDAO.updateTodo(todo);
     }
 
+    private static class DatabaseAsyncTask extends AsyncTask<Todo, Void, Void>
+    {
+        private TodoDAO todoDAO;
+        private boolean insert;
+        private boolean remove;
+        private boolean update;
 
+        public DatabaseAsyncTask(TodoDAO todoDAO, boolean insert, boolean remove, boolean update)
+        {
+            this.todoDAO = todoDAO;
+            this.insert = insert;
+            this.remove = remove;
+            this.update = update;
+        }
+
+        @Override
+        protected Void doInBackground(final Todo... todos)
+        {
+            for (Todo todo : todos)
+            {
+                if (insert)
+                {
+                    todoDAO.addTodo(todo);
+                }
+                if (update)
+                {
+                    todoDAO.updateTodo(todo);
+                }
+                if (remove)
+                {
+                    todoDAO.removeTodo(todo.getId());
+                }
+            }
+            return null;
+        }
+
+    }
 }
